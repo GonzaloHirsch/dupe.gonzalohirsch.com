97e46f6a229923324fb48002e971a934c41de211
GonzaloHirsch <hirschgonzalo@gmail.com>
2025-04-26 00:12:16 +0100

    feat: added processing of PHP response

--- api/src/controllers/controllers.ts
+++ api/src/controllers/controllers.ts
@@ -5,6 +5,8 @@ import { promisify } from 'util';
 import asyncHandler from 'express-async-handler';
 
 const exec = promisify(execCallback);
+const regexTest =
+  /.*\[(EMERGENCY|ALERT|CRITICAL|ERROR|WARNING|NOTICE|INFO|DEBUG)\].*/;
 
 export const runCommand = asyncHandler(
   async (req: Request, res: Response, _next: NextFunction) => {
@@ -22,7 +24,28 @@ export const runCommand = asyncHandler(
         console.error(`PHP stderr: ${stderr}`);
       }
 
-      res.send(stdout); // Waits until PHP finishes, then sends response
+      const tokenisedStdout = stdout
+        .split('\n')
+        .filter((line) => line.length > 0);
+      const logs = tokenisedStdout.filter((line) => regexTest.test(line));
+      const output = tokenisedStdout.filter((line) => !regexTest.test(line));
+
+      // Logging for visibility.
+      console.log('Received URL:', url);
+      console.debug(logs);
+      console.debug('Potential Output:', output);
+
+      let response: {
+        location?: string;
+      } = {};
+      try {
+        response['location'] = JSON.parse(output?.[0])?.result;
+      } catch (error: any) {
+        console.error(`Attempted to process ${output} but got ${error}.`);
+        res.send(response);
+      }
+
+      res.send(response);
     } catch (error: any) {
       console.error(`Error executing PHP:`, error);
       throw new Error(error.message);
