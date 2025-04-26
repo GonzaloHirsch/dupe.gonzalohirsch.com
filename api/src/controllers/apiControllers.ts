e01407ca1b7e2c2fcf6255e68c30e1987bda5611
GonzaloHirsch <hirschgonzalo@gmail.com>
2025-04-26 13:02:51 +0100

    feat: added base for find dupes

--- api/src/controllers/apiControllers.ts
+++ api/src/controllers/apiControllers.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import asyncHandler from 'express-async-handler';
 import { detectSchemaProduct } from './productControllers';
 import { gateOnURL } from '../utils/gate';
+import { isValidUrl } from '../utils/utils';
 
 export const detectProduct = asyncHandler(
   async (req: Request, res: Response, _next: NextFunction) => {
@@ -20,5 +21,45 @@ export const detectProduct = asyncHandler(
     // TODO: Map the schema product to a product model.
 
     res.status(200).send(schemaProduct);
+  },
+);
+
+export const findProductDupes = asyncHandler(
+  async (req: Request, res: Response, _next: NextFunction) => {
+    const { url } = req.body;
+
+    // Gating logic.
+    gateOnURL(url);
+
+    // TODO: Look for product in the DB. If we dont, say that we didn´t detect a product.
+    const isOriginalInDB = true;
+    if (!isOriginalInDB) {
+      res.status(404).send();
+    }
+
+    // TODO: Get the product from the DB.
+    // Probably limit the number of products to 10.
+    const potentialDupes: string[] = [];
+
+    // Performs the dupe detection on the rest. Make sure to filter out nulls.
+    const dupeProducts = (
+      await Promise.all(
+        potentialDupes
+          // Filter out invalid URLs.
+          .filter((url: string) => isValidUrl(url))
+          // Map the potential dupes to schema products with promises.
+          .map(async (potentialDupe: string) => {
+            return {
+              product: await detectSchemaProduct(potentialDupe),
+              url: potentialDupe,
+            };
+          }),
+      )
+    ).filter((dupe) => dupe !== null && dupe.product !== null);
+
+    // TODO: Store the results in the DB.
+    // TODO: Map the schema product to a product model.
+
+    res.status(204).send(dupeProducts);
   },
 );
--- api/src/routes/router.ts
+++ api/src/routes/router.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express';
-import { detectProduct } from '../controllers/apiControllers';
+import { detectProduct, findProductDupes } from '../controllers/apiControllers';
 
 const router = Router();
 
 router.post('/detect', detectProduct);
+router.post('/find', findProductDupes); // TODO: Maybe make this a GET request.
 
 export default router;
